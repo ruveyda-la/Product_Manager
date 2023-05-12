b6c2720f2728d6d1b9f07afc00f8564aca969612
Ruveyda <albayrakruveyda@gmail.com>
2023-05-11 22:55:58 -0500

    update

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import axios from 'axios';
+import {useState, useEffect} from 'react'
+import { useNavigate,Link } from 'react-router-dom'
 
 const Dashboard = () => {
+    const [state,setState]=useState([]);
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/products")
+             .then((res)=>{
+                setState(res.data)
+                navigate("/products")
+
+             })
+             .catch((err)=>{
+                console.log(err)
+             })
+    },[navigate])
+
   return (
-    <div>Dashboard</div>
+    <div className='container' style={{width:"1000px", marginTop:"100px"}}>
+        <h2>Products</h2>
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    state.map((item,idx) => (
+                        <tr key={idx}>
+                            <td>{item.title}</td>
+                            <td>{item.price}</td>
+                            <td>{item.description}</td>
+                            <td></td>
+                        </tr>
+                    ))
+
+
+                }    
+            </tbody>
+        </table>
+        <Link to="/products/new" >Add New Product</Link>
+
+
+
+    </div>
   )
 }
 
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+
+
 const Form= () => {
+    const navigate = useNavigate()
+    // const [errors,setErrors] = useState("")
     const [ product, setProduct ] = useState({
         title:"",
-        price:null,
+        price:"",
         description:""
     })
     const submitHandler =(e)=>{
-        e.preventDefault();
-        
-        axios.post("http://localhost:8000/api/products",{product})
+        e.preventDefault(); 
+        axios.post("http://localhost:8000/api/products",product)
             .then(res=>{console.log(res);
                         console.log(res.data);
-                        setProduct({
-                            title:"",
-                            price:null,
-                            description:""
+                        navigate("/products")
+                        // setProduct({
+                        //     title:"",
+                        //     price:null,
+                        //     description:""
+                        // })
                         })
-                        })
-            .catch(err=>console.log(err))
+            .catch((err) => {
+                console.log(err);
+                // setErrors(err.data)
+            })
         };
         
 
@@ -29,15 +37,15 @@ const Form= () => {
         })
     };
     return (
-        <div className='container'>
-            <form onSubmit={submitHandler}>
+        <div className='container' style={{width:"600px", marginTop:"100px"}}>
+            <form onSubmit={submitHandler} >
                 <label htmlFor="title">Title:</label>
-                <input type="text" name="title" onChange={changeHandler} className="form-control"/>
+                <input type="text" name="title" value={product.title} onChange={changeHandler}  className="form-control" style={{margin:"10px"}}/>
                 <label htmlFor="price">Price:</label>
-                <input type="number" name="price" onChange={changeHandler} className="form-control"/>
+                <input type="number" name="price" value={product.price} onChange={changeHandler} className="form-control" style={{margin:"10px"}}/>
                 <label htmlFor="title">Description:</label>
-                <input type="text" name="description" onChange={changeHandler} className="form-control"/>
-                <button type="button" className="btn btn-info">Create</button>
+                <input type="text" name="description" value={product.description} onChange={changeHandler} className="form-control" style={{margin:"10px"}}/>
+                <button type="submit" className="btn btn-info">Create</button>
             </form>
         </div>
     )
